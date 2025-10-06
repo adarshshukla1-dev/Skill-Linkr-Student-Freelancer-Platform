e913a36bc8a25ecf7633ec48c1f09b40499f27d9
adarshshukla1-dev <adarshshuklakota18@gmail.com>
2025-10-06 20:12:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,4 +4,15 @@ const navUl = document.querySelector('nav ul');
 menuToggle.addEventListener('click', () => {
   navUl.classList.toggle('show');
 });
+document.querySelectorAll('nav ul li a').forEach(link => 
+  {
+  link.addEventListener('click', function(e) 
+                        {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    target.scrollIntoView({ behavior: 'smooth' });
+    // Close mobile menu after click
+    navUl.classList.remove('show');
+  });
+});
 
